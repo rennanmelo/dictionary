8aeccb1c490854cc34abf344114b70b3d21d7a4d
Rennan Melo <rennan.melo11@gmail.com>
2020-03-28 07:35:31 -0300

    Create link to home page in create new word view. Create show word action, view and route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'words#index'
+  get '/words' => 'words#index'
   get '/words/new' => 'words#new'
   post '/words' => 'words#create'
+  get '/words/:id' => 'words#show', as: 'word'
 end
