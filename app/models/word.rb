000a9fe519572ce9df3b95d908157fcdc3fe3c64
Rennan Melo <rennan.melo11@gmail.com>
2020-03-30 05:27:40 -0300

    Implement case sensitive false to uniqueness rule

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ApplicationRecord
   validates :name, :meaning, :example, presence: true
-  validates :name, uniqueness: true
+  validates :name, :uniqueness => { :case_sensitive => false }
 
   extend FriendlyId
   friendly_id :name, use: :slugged
