af52452c399c14fb86c8819562a50e960fcef089
Rennan Melo <rennan.melo11@gmail.com>
2020-03-31 06:41:03 -0300

    Create limit to show 12 words at home and order it by created_at desc.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,9 @@
 class WordsController < ApplicationController
   def index
-    if params[:search]
-      @words = Word.where('name LIKE ?', "%#{params[:search]}%")
+    if params[:search] && params[:search] != ''
+      @words = Word.where('name LIKE ?', "%#{params[:search]}%").order(created_at: :desc)
     else
-      @words = Word.all
+      @words = Word.all.limit(12).order(created_at: :desc)
     end
   end
 
